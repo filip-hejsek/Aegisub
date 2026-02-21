filip-hejsek/Aegisub

commit 63992e298387be4ddcd344c693063409c520def5

Fix squished video on large video zoom
glViewport silently clamps viewport sizes to its internal caps, which
seem to be 16384 on most machines. If video zoom is implemented by
enlarging the viewport, this results in squished video on large video
zooms (which also only squishes the video and not the visual tools,
resulting in seemingly offset visual tools).

Fix this by applying the video zoom and pan via the projection matrix
instead and keeping the glViewport equal to the client size.

src/video_display.cpp
@@ -204,7 +204,8 @@ void VideoDisplay::Render() try {
 	wxSize client_size = GetClientSize();
 	client_size = wxSize(std::max(1, client_size.GetWidth()), std::max(1, client_size.GetHeight()));
 
-	videoOut->Render(viewport_left, viewport_bottom, viewport_width, viewport_height);
+	videoOut->Render(client_size.GetWidth() * scale_factor, client_size.GetHeight() * scale_factor,
+		viewport_left, viewport_bottom, viewport_width, viewport_height);
 	E(glViewport(0, 0, client_size.GetWidth() * scale_factor, client_size.GetHeight() * scale_factor));
 
 	E(glMatrixMode(GL_PROJECTION));

src/video_out_gl.cpp
@@ -167,8 +167,6 @@ void VideoOutGL::InitTextures(int width, int height, GLenum format, int bpp, boo
 
 	// Switch to video coordinates
 	CHECK_ERROR(glMatrixMode(GL_PROJECTION));
-	CHECK_ERROR(glLoadIdentity());
-	CHECK_ERROR(glPushMatrix());
 	if (frameFlipped) {
 		CHECK_ERROR(glOrtho(0.0f, frameWidth, 0.0f, frameHeight, -1000.0f, 1000.0f));
 	}
@@ -233,7 +231,6 @@ void VideoOutGL::InitTextures(int width, int height, GLenum format, int bpp, boo
 		}
 	}
 	CHECK_ERROR(glDisable(GL_TEXTURE_2D));
-	CHECK_ERROR(glPopMatrix());
 
 	glEndList();
 
@@ -267,9 +264,27 @@ void VideoOutGL::UploadFrameData(VideoFrame const& frame) {
 	CHECK_ERROR(glPixelStorei(GL_UNPACK_ROW_LENGTH, 0));
 }
 
-void VideoOutGL::Render(int dx1, int dy1, int dx2, int dy2) {
-	CHECK_ERROR(glViewport(dx1, dy1, dx2, dy2));
+void VideoOutGL::Render(int client_width, int client_height, int dx1, int dy1, int dx2, int dy2) {
+	CHECK_ERROR(glMatrixMode(GL_PROJECTION));
+	CHECK_ERROR(glLoadIdentity());
+	CHECK_ERROR(glPushMatrix());
+
+	float cw = static_cast<float>(client_width);
+	float ch = static_cast<float>(client_height);
+	// Transform (-1, -1) ~ (1, 1) rect to (-1, 1) + 2 * ( (dx1 / client_width, dx2 / client_height) ~ ((dx1 + dx2) / client_width, (dy1 + dy2) / client_height)) )) rect
+	// x = -1 goes to -1 + 2 * dx1 / cw
+	// x = +1 goes to -1 + 2 * (dx1 + dx2) / cw
+	float matrix[16] = {
+		dx2 / cw,		 0, 0, 0,
+		0,        dy2 / ch, 0, 0,
+		0, 0, 1, 0,
+		-1 + (2 * dx1 + dx2) / cw, -1 + (2 * dy1 + dy2) / ch, 0, 1,
+	};
+	CHECK_ERROR(glMultMatrixf(matrix));
+
 	CHECK_ERROR(glCallList(dl));
+
+	CHECK_ERROR(glPopMatrix());
 	CHECK_ERROR(glMatrixMode(GL_MODELVIEW));
 	CHECK_ERROR(glLoadIdentity());
 

src/video_out_gl.h
@@ -69,11 +69,13 @@ public:
 	void UploadFrameData(VideoFrame const& frame);
 
 	/// @brief Render a frame
+	/// @param width Width in physical pixels of client window
+	/// @param height Height in physical pixels of client window
 	/// @param x Bottom left x coordinate
 	/// @param y Bottom left y coordinate
 	/// @param width Width in pixels of viewport
 	/// @param height Height in pixels of viewport
-	void Render(int x, int y, int width, int height);
+	void Render(int client_width, int client_height, int x, int y, int width, int height);
 
 	VideoOutGL();
 	~VideoOutGL();

commit 1abfb9ec27a7d0b91871bb1d09ba88272ffb046e

Call glViewport() before videoOut->Render()
The code only worked because the viewport usually hasn't changed from the last time the frame was rendered.

The error was introduced by 63992e298387be4ddcd344c693063409c520def5.

src/video_display.cpp
@@ -218,9 +218,9 @@ void VideoDisplay::Render() try {
 	wxSize client_size = GetClientSize();
 	client_size = wxSize(std::max(1, client_size.GetWidth()), std::max(1, client_size.GetHeight()));
 
+	E(glViewport(0, 0, client_size.GetWidth() * scale_factor, client_size.GetHeight() * scale_factor));
 	videoOut->Render(client_size.GetWidth() * scale_factor, client_size.GetHeight() * scale_factor,
 		content_left, content_bottom, content_width, content_height);
-	E(glViewport(0, 0, client_size.GetWidth() * scale_factor, client_size.GetHeight() * scale_factor));
 
 	E(glMatrixMode(GL_PROJECTION));
 	E(glLoadIdentity());